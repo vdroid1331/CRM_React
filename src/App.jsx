4965a4dd12b7ee04cb178507604617b485dff2c8
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-09-03 16:40:09 +0530

    Added redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
 
 function App() {
-  return <Login />;
+  return <Signup />;
 }
 
 export default App;
