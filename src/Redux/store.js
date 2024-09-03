9956becfc07168c1ce7682d3e373d5547afb7041
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-09-03 16:47:40 +0530

    added auth slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from "./Slices/AuthSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authSliceReducer,
+  },
   devTools: true,
 });
 
