eabe288f1d69f152ac7a8049af0a091b796167de
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-09-09 02:23:30 +0530

    completed status cards

--- src/Redux/Slices/TicketSlice.js
+++ src/Redux/Slices/TicketSlice.js
@@ -5,6 +5,13 @@ import axiosInstance from "../../config/axiosInstance";
 
 const initialState = {
   ticketList: [],
+  ticketDistribution: {
+    open: 0,
+    inProgress: 0,
+    resolved: 0,
+    onHold: 0,
+    cancelled: 0,
+  },
 };
 
 export const getAllTicketsforTheUser = createAsyncThunk(
@@ -36,6 +43,18 @@ const ticketSlice = createSlice({
     builder.addCase(getAllTicketsforTheUser.fulfilled, (state, action) => {
       if (!action?.payload?.data) return;
       state.ticketList = action?.payload?.data?.result;
+      const tickets = action?.payload?.data?.result;
+      state.ticketDistribution = {
+        open: 0,
+        inProgress: 0,
+        resolved: 0,
+        onHold: 0,
+        cancelled: 0,
+      };
+      tickets.forEach((ticket) => {
+        state.ticketDistribution[ticket.status] =
+          state.ticketDistribution[ticket.status] + 1;
+      });
     });
   },
 });
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,25 +10,23 @@ function Card({
 }) {
   return (
     <div
-      className={`border-b-8 ${borderColor} w-64 h-44 ${background} rounded-md flex flex-col justify-center items-center py-2`}
+      className={`hover:scale-110 transition-all ease-out duration-300 border-b-8 ${borderColor} w-64 h-44 ${background} rounded-md flex flex-col justify-center items-center py-2`}
     >
       <div className="text-primary-content text-2xl mb-2">
         {children} <span>{titleText}</span>
       </div>
-
       <div className={`divider ${dividerColor} h-0.5 mx-4 rounded-sm`}></div>
 
       <div className="flex justify-around gap-4 items-center mt-2">
         <div className={`text-7xl ${fontColor}`}>{quantity}</div>
         <div
           className={`radial-progress ${fontColor}`}
-          style={{ "--value": status }}
+          style={{ "--value": status * 100 }}
         >
-          {status}%
+          {status * 100}%
         </div>
       </div>
     </div>
   );
 }
-
 export default Card;
