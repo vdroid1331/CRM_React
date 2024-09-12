96c11d99875b7ce98dd933b41ad9ea5ea2ec05ed
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-09-12 05:52:32 +0530

    Added user details page component

--- src/layouts/Homelayout.jsx
+++ src/layouts/Homelayout.jsx
@@ -42,6 +42,12 @@ function HomeLayout({ children }) {
               <Link to="/dashboard">Dashboard</Link>
             </li>
 
+            {authState.role === "admin" && (
+              <li>
+                <Link to="/users">All Users</Link>
+              </li>
+            )}
+
             <li className="absolute bottom-8 w-3/4">
               <div className="w-full flex justify-center items-center">
                 {!authState.isLoggedIn ? (
