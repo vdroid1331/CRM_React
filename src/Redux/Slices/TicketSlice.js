f6424143e114533e122ace2e80d55339ec4b50bd
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-09-13 15:23:39 +0530

    Added async thunk for creating new ticket

--- src/Redux/Slices/TicketSlice.js
+++ src/Redux/Slices/TicketSlice.js
@@ -36,6 +36,31 @@ export const getAllTicketsforTheUser = createAsyncThunk(
   }
 );
 
+export const createTicket = createAsyncThunk(
+  "tickets/createTicket",
+  async (ticket) => {
+    try {
+      const response = axiosInstance.post(
+        `ticket`,
+        ticket, // req body
+        {
+          headers: {
+            "x-access-token": localStorage.getItem("token"),
+          },
+        }
+      );
+      toast.promise(response, {
+        success: "Successfully created the ticket",
+        loading: "Creating the ticket",
+        error: "Something went wrong",
+      });
+      return await response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 export const updateTicket = createAsyncThunk(
   "tickets/updateTicket",
   async (ticket) => {
@@ -107,6 +132,23 @@ const ticketSlice = createSlice({
           if (ticket._id == updatedTicket._id) return updatedTicket;
           return ticket;
         });
+        state.ticketDistribution = {
+          open: 0,
+          inProgress: 0,
+          resolved: 0,
+          onHold: 0,
+          cancelled: 0,
+        };
+        state.downloadedTickets.forEach((ticket) => {
+          state.ticketDistribution[ticket.status] =
+            state.ticketDistribution[ticket.status] + 1;
+        });
+      })
+      .addCase(createTicket.fulfilled, (state, action) => {
+        if (action?.payload?.data == undefined) return;
+        const newTicket = action.payload.data;
+        state.downloadedTickets.push(newTicket);
+        state.ticketList = state.downloadedTickets;
         state.ticketDistribution = {
           open: 0,
           inProgress: 0,
--- src/pages/tickets/CreateTicket.jsx
+++ src/pages/tickets/CreateTicket.jsx
@@ -1,10 +1,58 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+
+import useTickets from "../../hooks/useTickets";
 import HomeLayout from "../../layouts/Homelayout";
+import { createTicket } from "../../Redux/Slices/TicketSlice";
 
 function CreateTicket() {
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useTickets();
+
+  const [ticket, setTicket] = useState({
+    title: "",
+    description: "",
+    ticketPriority: 3,
+    status: "open",
+    clientName: auth.data.clientName,
+  });
+
+  function handleFormChange(e) {
+    const { name, value } = e.target;
+    setTicket({
+      ...ticket,
+      [name]: value,
+    });
+  }
+
+  async function onFormSubmit(e) {
+    e.preventDefault();
+    if (!ticket.title || !ticket.description) {
+      toast.error("Title and description are mandatory");
+      return;
+    }
+    const response = await dispatch(createTicket(ticket));
+    if (response?.payload?.status == 201) {
+      // ticket got created successfully
+      setTicket({
+        title: "",
+        description: "",
+        ticketPriority: 3,
+        status: "open",
+        clientName: auth.data.clientName,
+      });
+    }
+  }
+
   return (
     <HomeLayout>
       <div className="min-h-[90vh] flex items-center justify-center">
-        <form className="min-w-[40rem] border p-20 border-sky-500 rounded-lg hover:bg-sky-900 transition-all ease-in-out duration-300">
+        <form
+          onSubmit={onFormSubmit}
+          className="min-w-[40rem] border p-20 border-sky-500 rounded-lg hover:bg-sky-900 transition-all ease-in-out duration-300"
+        >
           <h1 className="text-3xl font-semibold text-white text-center">
             Create new ticket
           </h1>
@@ -16,6 +64,9 @@ function CreateTicket() {
               </span>
             </label>
             <input
+              value={ticket.title}
+              onChange={handleFormChange}
+              name="title"
               type="text"
               placeholder="Type here"
               className="input input-bordered input-primary w-full bg-white text-black"
@@ -29,13 +80,19 @@ function CreateTicket() {
               </span>
             </label>
             <textarea
+              value={ticket.description}
+              onChange={handleFormChange}
+              name="description"
               placeholder="Type here"
               rows="8"
               className="p-2 resize-none w-full rounded-md bg-white text-black"
             ></textarea>
           </div>
 
-          <button className="w-full px-4 py-2 bg-green-500 text-lg font-semibold text-white rounded-md hover:bg-green-600 transition-all ease-in-out duration-300">
+          <button
+            type="submit"
+            className="w-full px-4 py-2 bg-green-500 text-lg font-semibold text-white rounded-md hover:bg-green-600 transition-all ease-in-out duration-300"
+          >
             Submit
           </button>
         </form>
